2da6cea076c2643c911ea2f012a3f262dd52e5bf
suparthghimire <suparthnarayanghimire2014@outlook.com>
2021-06-16 23:54:45 +0545

    Function for Canvas's Default Settings Created and Applied

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 const canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
-canvas.width = canvasDimension;
-canvas.height = canvasDimension;
+
+const defaultSettings = {
+  canvasDimension: 600,
+  strokeSize: 5,
+  bgColor: "#ffffff",
+  strokeColor: "#000000",
+};
+const set_bg_color = (color) => {
+  canvas.style.backgroundColor = color;
+};
+const apply_default_settings = () => {
+  canvas.width = defaultSettings.canvasDimension;
+  canvas.height = defaultSettings.canvasDimension;
+  document.querySelector("#current_stroke_size_value").textContent =
+    defaultSettings.strokeSize;
+  set_bg_color(defaultSettings.bgColor);
+};
+apply_default_settings();
